16c754db3982f2af53a8464a655fe13d15581b6d
Christie Hale <christie.d.hale@berkeley.edu>
2015-10-01 21:01:56 -0700

    initial commit

---
+++ SSC_Fractiles Files/fract.h
@@ -0,0 +1,10 @@
+c     Set array dimensions
+
+      PARAMETER ( MAX_SITE  = 1, MAX_FLT = 144, MAX_SEG  = 10,
+     1            MAX_INTEN = 12, MAX_PROB = 10, MAX_DIP=5,  
+     2            MAXPARAM = 150, MAX_MAG=15, MAX_DIST=15, 
+     3            MAX_EPS=15, MAX_N1=150, MAX_Files=3, MAX_N2=6,MAX_Xcost=10,
+     4            MAX_WIDTH=12, MAX_SAMPLE=10000, MAX_RISK=4000)
+      PARAMETER (MAXRUP=4, MAX_FIXED_MAG=3, MAX_MAGDIM=3, MAX_FTYPE=1, MAX_ATTEN=81, 
+     1            MAX_ATTENTYPE=1)
+
